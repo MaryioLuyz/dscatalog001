9cf7262fa55a576a0aace8edcf1a73918a6ad2c0
maryo <maryo@MARIO-LUIZ.com>
2026-09-04 23:24:26 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dscatalog001/Dscatalog001Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscatalog001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscatalog001Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatalog001/Dscatalog001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dscatalog001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

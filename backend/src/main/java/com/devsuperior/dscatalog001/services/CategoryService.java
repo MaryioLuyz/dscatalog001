87acee683d4281c6cbfa4c1572a6afb44df603dc
maryo <maryo@MARIO-LUIZ.com>
2026-09-11 16:21:12 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog001/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog001/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog001.services;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog001.entities.Category;
 import com.devsuperior.dscatalog001.repositories.CategoryRepository;
@@ -15,6 +16,7 @@ public class CategoryService {
 		this.repository = repository;
 	}
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
